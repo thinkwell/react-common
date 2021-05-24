dd2b8ebcb38b170a4a7db5a86f9ae1e9220e81a2
Dominik Steiner <dominik.j.steiner@gmail.com>
2021-05-24 11:13:55 -0600

    thinkwell/hippo#80 : allow opts for MainFrame#navigateTo

--- app/javascript/components/MainFrame.jsx
+++ app/javascript/components/MainFrame.jsx
@@ -2,18 +2,40 @@ import React, { useState, useEffect as useEffectReact, useCallback, useContext }
 import {Frame, TopBar, Card, ActionList, Loading, Navigation} from '@shopify/polaris';
 import {ProductsMajor, CustomersMajor, AffiliateMajor, ClockMajor, ReportsMajor} from '@shopify/polaris-icons';
 import {SearchContext} from './contexts/Search'
+import {PagingContext} from './contexts/Paging'
+import Util from './Util'
 import useEffect from './hooks/useEffect'
 
 export default function MainFrame(props) {
   const [searchValue, setSearchValue] = useContext(SearchContext);
+  const [page_info, previous_page_info, next_page_info, setPageInfo] = useContext(PagingContext);
   const [loading, setLoading] = useState(props.loading);
   const [mobileNavigationActive, setMobileNavigationActive] = useState(false);
 
   useEffect(() => { setLoading(props.loading) }, [props.loading])
 
-  const navigateTo = (href) => {
+  const navigateTo = (href, opts) => {
     if (href) {
-      window.location.assign(`/${href}`)
+      let path = `/${href}`;
+      const state = Util.getParam(window.location.href, 'state')
+      if (opts && opts.withContext == 'url') {
+        const page_info = Util.getParam(window.location.href, 'page_info')
+        if (page_info) {
+          path = Util.setParam(path, 'page_info', page_info)
+        }
+        const query = Util.getParam(window.location.href, 'query')
+        if (query) {
+          path = Util.setParam(path, 'query', query)
+        }
+      } else if (opts && opts.withContext) {
+        if (page_info) {
+          path = Util.setParam(path, 'page_info', page_info)
+        }
+        if (searchValue) {
+          path = Util.setParam(path, 'query', searchValue)
+        }
+      }
+      window.location.assign(path)
     }
   };
 
@@ -22,6 +44,7 @@ export default function MainFrame(props) {
   }
 
   const handleSearchFieldChange = useCallback((value) => {
+    setPageInfo('');
     setSearchValue(value);
     props.onSearchChange && props.onSearchChange(value)
   }, []);
