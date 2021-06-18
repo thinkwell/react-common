6bee93f9b2cd8d6759907bbf129f16b9d860feca
Dominik Steiner <dominik.j.steiner@gmail.com>
2021-06-18 08:20:16 -0600

    thinkwell/hippo#90 : remove deprecated EditSheet

--- packages/polaris/app/javascript/components/index.js
+++ packages/polaris/app/javascript/components/index.js
@@ -2,10 +2,9 @@ import AppProvider from './AppProvider'
 import MainFrame from './MainFrame'
 import ResourceList from './ResourceList'
 import EditModal from './EditModal'
-import EditSheet from './EditSheet'
 import EditForm from './EditForm'
 import TextField from './TextField'
 import Spinner from './Spinner'
 import Form from './Form'
 import Select from './Select'
-export {AppProvider, MainFrame, ResourceList, Form, EditModal, EditSheet, EditForm, TextField, Spinner, Select}
+export {AppProvider, MainFrame, ResourceList, Form, EditModal, EditForm, TextField, Spinner, Select}
