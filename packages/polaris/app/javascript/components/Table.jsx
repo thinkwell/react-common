916d925f03b0d122f18d1f2a4b457181317c7e08
Dominik Steiner <dominik.j.steiner@gmail.com>
2022-12-06 06:55:12 +0100

    add Table component

--- packages/polaris/app/javascript/components/Table.jsx
+++ packages/polaris/app/javascript/components/Table.jsx
@@ -6,7 +6,7 @@ export default function Table(props) {
   return (<table><tbody>
     {
       props.rows.map((row) => {
-        { !!row.value ? <tr>
+        return { !!row.value ? <tr>
           <th style={{textAlign: "left", verticalAlign: "top", padding: "5px"}}>{row.label}</th>
           <td style={{textAlign: "left", verticalAlign: "top", padding: "5px"}}>{row.valueLabel || row.value}</td>
         </tr> : null }
