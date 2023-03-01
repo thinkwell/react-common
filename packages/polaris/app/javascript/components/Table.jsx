67d169dcd4167e53b677cefacbc832880d5fde82
Dominik Steiner <dominik.j.steiner@gmail.com>
2023-03-01 06:34:48 +0100

    allow image display in Table

--- packages/polaris/app/javascript/components/Table.jsx
+++ packages/polaris/app/javascript/components/Table.jsx
@@ -6,9 +6,12 @@ export default function Table(props) {
   return (<table><tbody>
     {
       props.rows.map((row, index) => {
+        const value = row.valueLabel || row.value
         return !!row.value ? <tr key={`${row.label}_${index}`}>
           <th style={{textAlign: "left", verticalAlign: "top", padding: "5px"}}>{row.label}</th>
-          <td style={{textAlign: "left", verticalAlign: "top", padding: "5px"}}>{row.valueLabel || row.value}</td>
+          <td style={{textAlign: "left", verticalAlign: "top", padding: "5px"}}>
+            {row.image ? <img src={value} /> : value }
+          </td>
         </tr> : null
       })
     }
