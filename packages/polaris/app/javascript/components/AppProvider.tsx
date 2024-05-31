071ff7cb8d720b29f5da1419b6bc555a055d4eaf
Dominik Steiner <dominik.j.steiner@gmail.com>
2024-05-31 13:32:53 +0200

    thinkwell/shopper#806 : remove assert for enTranslations import

--- packages/polaris/app/javascript/components/AppProvider.tsx
+++ packages/polaris/app/javascript/components/AppProvider.tsx
@@ -1,6 +1,6 @@
 import React, { useState, ReactElement} from 'react';
 // @ts-ignore
-import enTranslations from '@shopify/polaris/locales/en.json' assert { type: "json" };
+import enTranslations from '@shopify/polaris/locales/en.json';
 import {AppProvider as AppProviderPolaris} from '@shopify/polaris';
 import { LinkLikeComponent } from '@shopify/polaris/build/ts/src/utilities/link';
 
--- packages/polaris/dist/AppProvider.js
+++ packages/polaris/dist/AppProvider.js
@@ -1,6 +1,6 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 // @ts-ignore
-import enTranslations from '@shopify/polaris/locales/en.json' assert { type: "json" };
+import enTranslations from '@shopify/polaris/locales/en.json';
 import { AppProvider as AppProviderPolaris } from '@shopify/polaris';
 export default function AppProvider(props) {
     return (_jsx(AppProviderPolaris, { i18n: enTranslations, linkComponent: props.linkComponent, children: props.children }));
