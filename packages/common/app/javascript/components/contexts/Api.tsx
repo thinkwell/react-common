0418f7c1b3de6ffd7be44d4bc3bf7ca5c8176d47
Dominik Steiner <dominik.j.steiner@gmail.com>
2024-07-03 11:55:02 +0200

    thinkwell/shopper#806 : allow for props.useFetcher in contexts/Api

--- packages/common/app/javascript/components/contexts/Api.tsx
+++ packages/common/app/javascript/components/contexts/Api.tsx
@@ -6,7 +6,7 @@ type ApiContextType = [useFetcher: boolean, setUseFetcher: (string) => void]
 const ApiContext = React.createContext<ApiContextType>([false, () => {}]);
 
 const ApiProvider = (props) => {
-  const [useFetcher, setUseFetcher] = useState<boolean>(false)
+  const [useFetcher, setUseFetcher] = useState<boolean>(props.useFetcher)
 
   return (
     <ApiContext.Provider value={[useFetcher, setUseFetcher]}>
--- packages/common/dist/contexts/Api.js
+++ packages/common/dist/contexts/Api.js
@@ -2,7 +2,7 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import React, { useState } from 'react';
 const ApiContext = React.createContext([false, () => { }]);
 const ApiProvider = (props) => {
-    const [useFetcher, setUseFetcher] = useState(false);
+    const [useFetcher, setUseFetcher] = useState(props.useFetcher);
     return (_jsx(ApiContext.Provider, { value: [useFetcher, setUseFetcher], children: props.children }));
 };
 export { ApiContext, ApiProvider };
