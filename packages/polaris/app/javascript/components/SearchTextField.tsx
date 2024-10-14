bfbcbb531c2824b019b18c0e47f604f9055a1fd3
Dominik Steiner <dominik.j.steiner@gmail.com>
2024-10-14 05:39:32 +0200

    thinkwell/shopper#806 : add clear icon to search text box

--- packages/polaris/app/javascript/components/SearchTextField.tsx
+++ packages/polaris/app/javascript/components/SearchTextField.tsx
@@ -1,13 +1,19 @@
 import React, { useContext, useRef } from 'react';
 import {SearchContext, useSearch} from '@thinkwell/react.common';
 import {TextField, Icon} from '@shopify/polaris';
-import {SearchIcon} from '@shopify/polaris-icons';
+import {SearchIcon, XCircleIcon} from '@shopify/polaris-icons';
 
 export default function SearchTextField(props) {
   const [query] = useContext(SearchContext)
   
   const onSearch = props.onSearch || function(){}
   const [onSearchChange] = useSearch(onSearch)
+
+  const onClear = () => {
+    if (!!query) {
+      onSearchChange("")
+    }
+  }
 
   return (
     <div>
@@ -17,6 +23,7 @@ export default function SearchTextField(props) {
         labelHidden={true}
         onChange={onSearchChange}
         value={query}
+        suffix={<div onClick={onClear}><Icon source={XCircleIcon} tone="base" /></div>}
         prefix={<Icon source={SearchIcon} tone="base" />}
         placeholder={props.placeholder}
       />
--- packages/polaris/dist/SearchTextField.js
+++ packages/polaris/dist/SearchTextField.js
@@ -2,10 +2,15 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { useContext } from 'react';
 import { SearchContext, useSearch } from '@thinkwell/react.common';
 import { TextField, Icon } from '@shopify/polaris';
-import { SearchIcon } from '@shopify/polaris-icons';
+import { SearchIcon, XCircleIcon } from '@shopify/polaris-icons';
 export default function SearchTextField(props) {
     const [query] = useContext(SearchContext);
     const onSearch = props.onSearch || function () { };
     const [onSearchChange] = useSearch(onSearch);
-    return (_jsx("div", { children: _jsx(TextField, { autoComplete: 'off', label: "", labelHidden: true, onChange: onSearchChange, value: query, prefix: _jsx(Icon, { source: SearchIcon, tone: "base" }), placeholder: props.placeholder }) }));
+    const onClear = () => {
+        if (!!query) {
+            onSearchChange("");
+        }
+    };
+    return (_jsx("div", { children: _jsx(TextField, { autoComplete: 'off', label: "", labelHidden: true, onChange: onSearchChange, value: query, suffix: _jsx("div", { onClick: onClear, children: _jsx(Icon, { source: XCircleIcon, tone: "base" }) }), prefix: _jsx(Icon, { source: SearchIcon, tone: "base" }), placeholder: props.placeholder }) }));
 }
